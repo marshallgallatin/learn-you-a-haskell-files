03cf9f0db9a5145e1a3ace6fe29f565930b49f00
Marshall Gallatin <marshallgallatin@gmail.com>
2018-08-29 18:24:21 -0500

    added some functions to play around with

--- scratch-space.hs
+++ scratch-space.hs
@@ -5,3 +5,10 @@ doubleUs x y = x*2 + y*2
 doubleSmallNumber x = if x > 100
                         then x
                         else x*2
+
+doubleUs2 x y = doubleMe x + doubleMe y
+
+collatz x = if odd x then 3*x+1 else x `div` 2
+
+factorial :: Integer -> Integer
+factorial n = product [1..n]
